9681477d6db98a10c21c8b36295489ac0efd8339
Will <code@willo.info>
2014-07-14 20:33:43 +1000

    stripped back

--- transmitter_jun21.ino
+++ transmitter_jun21.ino
@@ -1,5 +1,4 @@
 #include <VirtualWire.h>
-#include "LowPower.h"
 
 #undef round
 int lightPin = 0; 
@@ -24,8 +23,7 @@ void setup()
 
 void loop()
 {
-    Serial.println("Start Loop");
-    delay(1000);
+    delay(2000);
     int lightPinC1 = analogRead(lightPin);
     int tempPinC1 = analogRead(tempPin);
     char msg[24];
@@ -35,34 +33,18 @@ void loop()
     //If light level has changed by the above percentage, then send via radio. Otherwise, go back to sleep.
     //This is to save power. There's no point transmitting the same thing.
     
-    if (lightPinC1 < (lastLight - percentCheck) || lightPinC1 > (lastLight + percentCheck))
-    {
-      int RadioCount = 0;
-      randomSeed(analogRead(3));
-      while(RadioCount < 3)
-      {
-        int randNumber = random(10, 850);
-        sprintf(msg, "S:1#L:%i#T:%i#X:XXX#C:%i", lightPinC1,tempPinC1,count);
-        Serial.println(msg);
-        digitalWrite(13, true);
-        vw_send((uint8_t *)msg, strlen(msg));
-        vw_wait_tx();
-        digitalWrite(13, false);
-        RadioCount++;
-        Serial.print("Random: ");
-        Serial.println(randNumber);
-        delay(randNumber);
-      }
-      
-      //Don't let the counter get over 999
-      if (count == 999)
-      {
-        count = 0;
-      }  
-      count++;
-    }
-
-    lastLight = lightPinC1;
-    LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, 
-                  SPI_OFF, USART0_OFF, TWI_OFF);               
+    int RadioCount = 0;
+    randomSeed(analogRead(3));
+    int randNumber = random(10, 850);
+    sprintf(msg, "S:1#L:%i#T:%i#R:%i#C:%i", lightPinC1,tempPinC1,randNumber,count);
+    Serial.println(msg);
+    digitalWrite(13, true);
+    vw_send((uint8_t *)msg, strlen(msg));
+    vw_wait_tx();
+    digitalWrite(13, false);
+    RadioCount++;
+    Serial.print("Random: ");
+    Serial.println(randNumber);
+ 
+    lastLight = lightPinC1;            
 }
