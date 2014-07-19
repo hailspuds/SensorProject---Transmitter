8e43d2483c0538a04a54c98e52ed65e03d9a6f27
Will <code@willo.info>
2014-07-19 12:28:30 +1000

    testing with 10

--- transmitter_jun21.ino
+++ transmitter_jun21.ino
@@ -1,7 +1,7 @@
 #include <VirtualWire.h>
 
 int count = 1;
-int numbers[3]; // Change 3 to number of integers you wish to send.
+int numbers[10]; // Change 3 to number of integers you wish to send.
 
 void setup()
 {
@@ -10,23 +10,27 @@ void setup()
 
   vw_set_tx_pin(3);
   vw_set_ptt_inverted(true);
-  vw_setup(1200); // Bits per sec
-
-  // Initialize to some sample values
-  numbers[0] = 32767;
-  numbers[1] = -2;
-  numbers[2] = 0;
+  vw_setup(1200); // Bits per sec  
 }
 
 void loop()
 {
+  // Initialize to some sample values
+  numbers[0] = 32767;
+  numbers[1] = -2;
+  numbers[2] = 0;
+  numbers[3] = count;
+  numbers[4] = count;
+  numbers[5] = count;
+  numbers[6] = count;
+  numbers[7] = count;
+  numbers[8] = count;
+  numbers[9] = 9999;
+  
   vw_send( (uint8_t *)numbers, sizeof(numbers));
   vw_wait_tx();  // Wait for message to finish
   delay(1000);
 
-  numbers[0]--;  // decrement
-  numbers[1]++;  // increment
-  numbers[2]++;  // increment
   count++;    
 }
 
